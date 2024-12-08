0e56679096cfc93aa30a4b392b1546a370bf15f1
BachtiarYsf <bachtiar2331096@itpln.ac.id>
2024-12-09 06:52:25 +0700

    Add files via upload

---
+++ asset/java.js
@@ -0,0 +1,35 @@
+function sembunyi() {
+    var list = document.getElementById("hil");
+    if (list.style.display === "none" || list.style.display === "") {
+        list.style.display = "block";
+    } else {
+        list.style.display = "none";
+    }
+}
+
+function bagianA() {
+    var list = document.getElementById("A");
+    if (list.style.display === "none" || list.style.display === "") {
+        list.style.display = "block";
+    } else {
+        list.style.display = "none";
+    }
+}
+
+function bagianB() {
+    var list = document.getElementById("B");
+    if (list.style.display === "none" || list.style.display === "") {
+        list.style.display = "block";
+    } else {
+        list.style.display = "none";
+    }
+}
+
+function bagianC() {
+    var list = document.getElementById("C");
+    if (list.style.display === "none" || list.style.display === "") {
+        list.style.display = "block";
+    } else {
+        list.style.display = "none";
+    }
+}
